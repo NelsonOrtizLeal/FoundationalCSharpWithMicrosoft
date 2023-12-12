5d922442086cafc5dcd9b188b2314e7eb9ae9edc
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2023-12-11 23:09:54 -0600

    Incorporacion de una instruccion if para mostrar diferentes mensajes segun el valor de la variable total

--- 3.-Conditon IF/CsharpProjects/TestProject/Program.cs
+++ 3.-Conditon IF/CsharpProjects/TestProject/Program.cs
@@ -6,4 +6,10 @@ int roll3 = dice.Next(1,7);
 
 int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if( total > 14){
+    Console.WriteLine("You win!");
+}else{
+    Console.WriteLine("Sorry, you lose.");
+}
